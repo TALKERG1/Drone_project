6d3819c31755037f70e4eeda493c48c1b2138974
TALKERG1 <105305279+TALKERG1@users.noreply.github.com>
2022-05-12 09:44:39 -0700

    Update Beef Drone.py

--- Beef Drone.py
+++ Beef Drone.py
@@ -1,1 +1,34 @@
+from djitellopy import tello
+from time import sleep
+
+beef = tello.Tello()
+beef.connect()
+print(beef.get_battery())
+
+beef.takeoff()
+
+# Starting point
+beef.move_up(80)
+beef.move_forward(500)
+sleep(.5)
+
+# Turning into "Must fly zone"
+beef.rotate_counter_clockwise(90)
+beef.move_forward(300)
+sleep(.5)
+
+# Going back to start
+beef.rotate_counter_clockwise(90)
+beef.move_forward(500)
+sleep(.5)
+
+# on spot
+beef.rotate_counter_clockwise(90)
+beef.move_forward(300)
+beef.rotate_counter_clockwise(90)
+
+# stop flying
+sleep(.5)
+
+beef.land()
 
